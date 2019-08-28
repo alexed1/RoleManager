62409520667d55bc2d796906b775c9a739621517
Andrii Kraiev <andkraiev@gmail.com>
2019-08-28 19:14:15 +0300

    Initial commit

---
+++ force-app/main/default/lwc/roleManager/roleManager.js
@@ -0,0 +1,98 @@
+import {LightningElement, api, wire, track} from 'lwc';
+
+import SupportedButtonsEmptyMessage from '@salesforce/label/c.SupportedButtonsEmptyMessage';
+import SupportedAddCapabilitiesEmptyMessage from '@salesforce/label/c.SupportedAddCapabilitiesEmptyMessage';
+import ButtonIsNotSupportedMessage from '@salesforce/label/c.ButtonIsNotSupportedMessage';
+import SupportedEditCapabilitiesEmptyMessage from '@salesforce/label/c.SupportedEditCapabilitiesEmptyMessage';
+import ManagerEmptyMessage from '@salesforce/label/c.ManagerEmptyMessage';
+
+import {refreshApex} from '@salesforce/apex';
+import getExistingMembers from '@salesforce/apex/RoleManagerController.getExistingMembers';
+import getSupportedButtons from '@salesforce/apex/RoleManagerController.getSupportedButtons';
+
+import {logger, logError} from 'c/lwcLogger';
+
+import {
+    getNotSupportedButtons
+} from 'c/buttonUtils';
+
+
+export default class RoleManager extends LightningElement {
+    @api recordId;
+    @api editTabName;
+    @api addTabName;
+    @api availableObjectTypes;
+    @api managerName;
+    @api supportedAddCapabilities;
+    @api supportedEditCapabilities;
+    @track existingMembers;
+    @track supportedButtons;
+    @api ruleName;
+    _refreshable;
+    @api log = false;
+    source = 'RoleManager';
+    @track loadFinished = false;
+    @track cardTitle = '';
+
+    @wire(getSupportedButtons, {managerName: '$managerName'})
+    _getSupportedButtons(result) {
+        this._refreshable = result;
+        if (result.error) {
+            logError(this.log, this.source, 'getSupportedButtons', result.error);
+        } else if (result.data) {
+            this.supportedButtons = JSON.parse(result.data);
+            this.loadFinished = true;
+        }
+    }
+
+    @wire(getExistingMembers, {managerName: '$managerName', recordId: '$recordId'})
+    _getExistingMembers(result) {
+        this._refreshable = result;
+        if (result.error) {
+            logError(this.log, this.source, 'getExistingMembers', result.error);
+        } else if (result.data) {
+            this.existingMembers = result.data;
+        }
+    }
+
+    @api refresh() {
+        refreshApex(this._refreshable).then(result => {
+            let cmpToRefresh = this.template.querySelector('c-add-new-members');
+            if (cmpToRefresh) {
+                cmpToRefresh.updateRowButtons();
+            }
+        });
+    }
+
+    get errorMessage() {
+        let resultErrors = [];
+
+        if (!this.supportedButtons) {
+            resultErrors.push(SupportedButtonsEmptyMessage);
+        }
+        if (!this.supportedAddCapabilities) {
+            resultErrors.push(SupportedAddCapabilitiesEmptyMessage);
+        }
+        if (!this.supportedEditCapabilities) {
+            resultErrors.push(SupportedEditCapabilitiesEmptyMessage);
+        }
+        if (!this.managerName) {
+            resultErrors.push(ManagerEmptyMessage);
+        }
+
+        if (this.supportedButtons && (this.supportedAddCapabilities || this.supportedEditCapabilities)) {
+            let notSupportedButtnos = getNotSupportedButtons(this.supportedButtons, this.supportedAddCapabilities + ', ' + this.supportedEditCapabilities);
+
+            if (notSupportedButtnos.length > 0) {
+                resultErrors.push(ButtonIsNotSupportedMessage + notSupportedButtnos.join(', '));
+            }
+        }
+
+        if (resultErrors.length) {
+            return resultErrors.join('; ');
+        } else {
+            return false;
+        }
+
+    }
+}
